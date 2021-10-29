f6b43f7d3fbb6f6414405eda9a9e1ab05e7a12a1
santosh78-sys <61026956+santosh78-sys@users.noreply.github.com>
2021-10-29 17:36:19 +0530

    Add files via upload

---
+++ Components/EditableRow.js
@@ -0,0 +1,82 @@
+
+import React from "react";
+
+const EditableRow = ({
+  editFormData,
+  handleEditFormChange,
+  handleCancelClick,
+}) => {
+  return (
+    <tr>
+      <td>
+        <input
+     className="fa fa-fw fa-user"
+          type="text"
+          required="required"
+          placeholder="Enter a name..."
+          name="fullName"
+          value={editFormData.fullName}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter source"
+          name="source"
+          value={editFormData.source}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter an address..."
+          name="address"
+          value={editFormData.address}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="text"
+          required="required"
+          placeholder="Enter a phone number..."
+          name="Source"
+          value={editFormData.phoneNumber}
+          onChange={handleEditFormChange}
+        ></input>
+      </td>
+      <td>
+        <input
+          type="email"
+          required="required"
+          placeholder="Enter an email..."
+          name="Phone Number"
+          value={editFormData.email}
+          onChange={handleEditFormChange}
+        ></input>
+        </td>
+        <td>
+         <input
+          type="Source"
+          required="optional"
+          placeholder="Enter the source..."
+          name="source"
+          value={editFormData.Source}
+          onChange={handleEditFormChange}
+        ></input>
+        </td>
+        <td>
+        <button type="submit">Save</button>
+        <button type="button" onClick={handleCancelClick}>
+          Cancel
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default EditableRow;
